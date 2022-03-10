370eeaa573e4608cdeb6524440efbe3d6bf0f7a0
ItsMeRadu <imagabbs@gmail.com>
2022-03-10 23:56:01 +0200

    Initial commit

---
+++ Computer.py
@@ -0,0 +1,70 @@
+from Player import Player
+from Ship import Ship
+import random
+
+
+class Computer(Player):
+
+    def __init__(self):
+        super().__init__(self)
+        self.name = "Computer"
+
+    # Automated version of set_fleet function from Player
+
+    def set_compu_fleet(self):
+        positions = ["v", "h"]
+
+        for ship, size in self.ships.items():
+
+            flag = True
+            while flag:
+                row = random.randint(0, 9)
+                col = random.randint(0, 9)
+                orientation = random.choice(positions)
+
+                if orientation == "v":
+                    if self.ocean.can_use_row(row, col, size):
+                        self.ocean.set_ship_row(row, col, size)
+                        boat = Ship(ship, size)
+                        boat.plot_vertical(row, col)
+                        self.fleet.append(boat)
+                        flag = False
+
+                    else:
+                        row = row + 2
+
+                elif orientation == "h":
+                    if self.ocean.can_use_col(row, col, size):
+                        self.ocean.set_ship_col(row, col, size)
+                        boat = Ship(ship, size)
+                        boat.plot_horizontal(row, col)
+                        self.fleet.append(boat)
+                        flag = False
+
+                    else:
+                        col = col + 2
+
+                else:
+                    continue
+
+    # Automated strike function
+
+    def compu_strike(self, target):
+        row = random.randint(0, 9)
+        col = random.randint(0, 9)
+
+        if self.radar[row][col] == ".":
+            input("Target acquired.%s, %s" % (row, col))
+
+            if target.ocean[row][col] == "S":
+                print("Hit!")
+                target.ocean[row][col] = "X"
+                target.register_hit(row, col)
+                self.radar[row][col] = "X"
+
+            else:
+                print("Missed!")
+                self.radar[row][col] = "O"
+
+        else:
+            self.compu_strike(target)
